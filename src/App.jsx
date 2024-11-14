c9fc200509ef74adfec0584133381f5c9349cf08
montedeu <qblcrank@gmail.com>
2024-11-15 03:08:47 +0500

    ommiting only changed files

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import {PropTypes} from "prop-types";
 
 function Square({ value, onSquareClick }) {
-
     return (<button className={'square'} onClick={onSquareClick}>
         {value}
     </button>)
